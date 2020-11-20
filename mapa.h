498b2304f0f914a250a0a4bf8e851ef7ec3a44a3
AgathaGoncalves <71910283+AgathaGoncalves@users.noreply.github.com>
2020-11-19 22:23:08 -0300

    Update mapa.h

--- mapa.h
+++ mapa.h
@@ -31,3 +31,47 @@ void exibirLab(int mapa[5][5],int linhaInicial, int colunaInicial, int linhaFina
     cout << endl;
 
 }
+void obtemLabirinto(int M[MAX][MAX], int *dimLinhas, int *dimColunas,int *linhaInicial, int *colunaInicial, int *linhaFinal, int *colunaFinal) {
+int i, j, d;
+    *dimLinhas = 5;
+    *dimColunas = 5;
+
+    *linhaInicial = 1;
+    *colunaInicial = 1; 
+ 
+    *linhaFinal = 1;
+    *colunaFinal = 5;
+
+    for (i = 0; i < *dimLinhas; i++)
+        for (j = 0; j < *dimColunas; j++) {
+            if (M[i][j] == 1)
+                M[i][j] = -2;
+            else
+                M[i][j] = -1;
+        }
+}
+int labirinto(int M[MAX][MAX], int asLinhas[], int asColunas[],int linha, int coluna, int Lf, int Cf, char mapaS[MAX][MAX]) {
+
+    int L, C, k, passos;
+    
+
+    if ((linha == Lf) && (coluna == Cf)) 
+        return M[linha][coluna];
+        
+    for (k = 0; k < 4; k++) {
+        L = linha + asLinhas[k];
+        C = coluna + asColunas[k];
+
+        if (M[L][C] == -1) { 
+            M[L][C] = M[linha][coluna] + 1;
+            moverRato(mapaS, L, C);
+            passos = labirinto(M, asLinhas, asColunas, L, C, Lf, Cf, mapaS);
+            if (passos > 0) 
+                return passos;
+        }
+
+    } 
+
+    return 0;
+    }
+    #endif
