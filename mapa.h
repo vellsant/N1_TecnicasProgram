4654e5dd9f68e7f725a9d4ca0761098e0356824d
jvitoram <74027816+jvitoram@users.noreply.github.com>
2020-11-19 21:53:22 -0300

    Update mapa.h

--- mapa.h
+++ mapa.h
@@ -1,33 +1,33 @@
+#ifndef mapa_h
+#define mapa_h
+
+#include "rato.h"
 #include <iostream>
-#include <cstdlib>
-#include <time.h>
+using namespace std; 
+#define MAX 5
 
-using namespace std;
-
-void desenhaMapa(){
-    int portaX = 0;
-    int portaY = 0;
-
-    string mapa[5][5] = {
-    {"O ","O ","# ","O ","O "},
-    {"O ","O ","# ","O ","O "},
-    {"O ","O ","# ","O ","O "},
-    {"O ","O ","# ","O ","O "},
-    {"O ","O ","O ","O ","O "},
-    };
-
-    srand(time(0));
-    portaX = rand() % 5;
-    portaY = rand() % 5;
-
-    mapa[portaX][portaY] = "P ";
-
-    for (int i = 0; i < 5; i++)
-        {
-        for(int j = 0; j <5; j++){
-        cout << mapa[i][j];
+void exibirLab(int mapa[5][5],int linhaInicial, int colunaInicial, int linhaFinal, int colunaFinal, char mapaS[5][5]){
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if(i == linhaInicial-1 && j == colunaInicial-1){ 
+                mapaS[i][j] = 'E';
+                cout <<" E/R ";
+            }
+            else if(i == linhaFinal-1 && j == colunaFinal-1){ 
+                mapaS[i][j] = 'Q';
+                cout <<"  Q  ";
+            }
+            else if (mapa[i][j] == -2){
+                mapaS[i][j] = 'P';
+                cout <<"  P  ";
+            }
+            else if (mapa[i][j] == -1){ 
+                mapaS[i][j] = '.';
+                cout <<"  .  ";
+            }   
         }
         cout << endl;
     }
-}
+    cout << endl;
 
+}
